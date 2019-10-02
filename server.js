805290535f590d80dddf2f3b008a3d2baafc8c8c
Eric Nguyen <enguyen89141@gmail.com>
2019-10-01 23:36:54 -0700

    finished assignment and checked against solution

---
+++ server.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const MOVIES = require('./movies-data.json')
+
+console.log(process.env.API_TOKEN)
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request'})
+    }
+    next()
+})
+
+app.get('/movie', (req, res) => {
+    let response = MOVIES
+    if (req.query.genre) {
+        response = response.filter(movie => 
+            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase()))
+    }
+
+    if (req.query.country) {
+        response = response.filter(movie => 
+            movie.country.toLowerCase().includes(req.query.country.toLowerCase()))
+            const stockNumbers = Array(20).fill(1).map((_, i) => i + 1)
+    }
+
+    if (req.query.avg_vote) {
+        parseFloat(req.query.avg_vote)
+        response = response.filter(movie => 
+            movie.avg_vote >= req.query.avg_vote )
+    }
+
+    res.json(response)
+})
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`)
+});
